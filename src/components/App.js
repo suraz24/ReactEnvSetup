bf50504af22fc95656541ec03c3b7cefa7dc94e3
Krishna Adhikari <kadhikar@unico-aus.unico.com.au>
2017-09-22 12:04:03 +1000

    Initial setup commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, {PropTypes} from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Hello World</h1>
+      </div>);
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const HotModuleReplacementPlugin = new webpack.HotModuleReplacementPlugin();
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: 'bundle.js',
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+      HtmlWebpackPluginConfig,
+      HotModuleReplacementPlugin
+  ]
+}
